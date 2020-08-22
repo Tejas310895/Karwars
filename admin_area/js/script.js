f5c32bc92344b6f7d39bfa0355313d38ccfda573
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-08-23 02:18:50 +0530

    error

--- admin_area/js/script.js
+++ admin_area/js/script.js
@@ -33,4 +33,37 @@ $(document).ready(function () {
 
     });
 
+    $(".view_report").click(function (e) { 
+        e.preventDefault();
+        
+        var show= $(this).attr("type");
+        var start= $('.start_date').val();
+        var end= $('.end_date').val();
+        var status= $('.status').val();
+        
+        if(show!=''&&start!=''&&end!=''){
+
+            if(Date.parse(start) > Date.parse(end)){
+                alert("Invalid Date Range");
+             }
+             else{
+
+                $.ajax({
+                    type: 'post',
+                    url: 'stock_data.php',
+                    datatype: "json",
+                    data: {show:show,start:start,end:end,status:status},
+                    success: function (response) 
+                    {
+                        $("#report_date").html(response);
+                        $(".download_report").removeClass("d-none");
+                    }
+                });
+
+            }
+
+        }
+
+    });
+
 });
