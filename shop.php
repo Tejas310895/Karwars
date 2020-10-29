e87f9b5a47cedda501a752056e94ac31385800f8
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-10-29 15:46:09 +0530

    error

--- shop.php
+++ shop.php
@@ -212,7 +212,7 @@
                         $pro_stock = $row_products['product_stock'];
 
                         if($price_display>0){
-                            $discount_percent = round(($pro_price/$price_display)*100);
+                            $discount_percent = 100-round(($pro_price/$price_display)*100);
                         }else{
                             $discount_percent = 0;
                         }
@@ -220,9 +220,9 @@
                         
                             <div class="row bg-white mt-1 py-2" id="<?php echo $pro_id;?>">
                                     <div class="col-4">
-                                        <!-- <span class="notify-badge <?php if($price_display>0){echo "show";}else{echo "d-none";}?>">
+                                        <span class="notify-badge <?php if($price_display>0){echo "show";}else{echo "d-none";}?>">
                                         <h5 class="pro_dis_batch mb-0"><?php echo $discount_percent."%"; ?> </h5>
-                                        </span> -->
+                                        </span>
                                         <img src="<?php echo $pro_img1; ?>" alt="..." class="img-thumbnail border-0">
                                     </div>
                                 <div class="col-8">
