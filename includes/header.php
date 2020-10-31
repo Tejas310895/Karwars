2a69bb9b81fcd82d5c90463c114ab3d9addefc68
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-10-31 23:45:54 +0530

    offer updates

--- includes/header.php
+++ includes/header.php
@@ -39,7 +39,7 @@ if(isset($_GET['pro_id'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="description" content="The best online grocery store in Karwar. We can fulfill all your grocery needs ranging from grains,pulses,kitchen needs to fresh vegetables and fruits"/>
     <link rel="shortcut icon" type="image/png" href="../admin_area/admin_images/wrnlogo.png"/>
-    <title>Karwar Grocery</title>
+    <title>Karwars Grocery</title>
     <!-- google font -->
     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Jost' rel='stylesheet'>
