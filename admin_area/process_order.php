bef1a71c2bcc028012bcff44450fd248c45222ac
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-02-06 15:22:19 +0530

    del_date

--- admin_area/process_order.php
+++ admin_area/process_order.php
@@ -4,11 +4,14 @@ include("includes/db.php");
 
 if(isset($_GET['update_order'])){
 
+  date_default_timezone_set('Asia/Kolkata');
+  $today = date("Y-m-d H:i:s");
+
   $update_order = $_GET['update_order'];
 
   $status = $_GET['status'];
 
-  $update_status_del = "UPDATE customer_orders SET order_status='$status' WHERE invoice_no='$update_order'";
+  $update_status_del = "UPDATE customer_orders SET order_status='$status',del_date='$today' WHERE invoice_no='$update_order'";
 
   $run_status_del = mysqli_query($con,$update_status_del);
 
