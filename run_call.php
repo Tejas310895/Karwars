5bde5a4a96b79ceee368f1f61cec4321a9217420
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-02-17 15:53:21 +0530

    error

--- run_call.php
+++ run_call.php
@@ -17,6 +17,7 @@
 
         $get_waclient = "SELECT DISTINCT(client_id) FROM customer_orders WHERE invoice_no='$invoice_no'";
         $run_waclient = mysqli_query($con,$get_waclient);
+        $rows = array();
         while($row_waclient=mysqli_fetch_array($run_waclient)){
         $waclient_id = $row_waclient['client_id'];
         
@@ -24,21 +25,46 @@
         $run_wacontact = mysqli_query($con,$get_wacontact);
         $row_wacontact = mysqli_fetch_array($run_wacontact);
         
+        array_push($rows, $row_wacontact['client_phone']);
         
-        $waclient_phone = $row_wacontact['client_phone'];
+        // $waclient_phone = $row_wacontact['client_phone'];
+
         
-        $username = urlencode("r701");
-        $token = urlencode("oV426q");
-        $plan_id = urlencode("10221");
-        $announcement_id = urlencode("242413");
-        $caller_id = urlencode("12345");
-        $contact_numbers = urlencode("$waclient_phone");
+        // $username = urlencode("r701");
+        // $token = urlencode("oV426q");
+        // $plan_id = urlencode("10221");
+        // $announcement_id = urlencode("242413");
+        // $caller_id = urlencode("12345");
+        // $contact_numbers = urlencode("$waclient_phone");
 
-        $api = "http://103.255.100.37/api/voice/voice_broadcast.php?username=".$username."&token=".$token."&plan_id=".$plan_id."&announcement_id=".$announcement_id."&caller_id=".$caller_id."&contact_numbers=".$contact_numbers."";
+        // $api = "http://103.255.100.37/api/voice/voice_broadcast.php?username=".$username."&token=".$token."&plan_id=".$plan_id."&announcement_id=".$announcement_id."&caller_id=".$caller_id."&contact_numbers=".$contact_numbers."";
 
-        $response = file_get_contents($api);
+        // $response = file_get_contents($api);
 
         }
+
+        $string = implode(',' , $rows);
+
+        $post_url = "http://103.255.100.37/api/voice/voice_broadcast.php";
+
+        $ARR_POST_DATA = array();
+
+        $ARR_POST_DATA['username'] = 'r701';
+        $ARR_POST_DATA['token'] = "oV426q";
+        $ARR_POST_DATA['announcement_id'] = "242413";
+        $ARR_POST_DATA['plan_id'] = 10221;
+        $ARR_POST_DATA['caller_id'] = "1234"; // optional
+        $ARR_POST_DATA['contact_numbers'] = "$string"; // comma(,) seperated 
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $post_url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($ARR_POST_DATA));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $response = curl_exec($curl);
+        // $arr_response = json_docode($response, true);
+        // var_dump($arr_response);
+        // curl_close($curl); 
 
         if($response){
 
