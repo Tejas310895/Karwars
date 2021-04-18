01c6d6be8e598721b3af3a2a4fc27f058833e673
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-04-18 20:58:36 +0530

    error

--- admin_area/dashboard.php
+++ admin_area/dashboard.php
@@ -9,6 +9,11 @@
         ?>
 <?php 
 
+
+date_default_timezone_set('Asia/Kolkata');
+$today = date("Y-m-d");
+// $current_date = date('Y-m-d',strtotime($today));
+
 $get_total_sales = "SELECT sum(due_amount) AS total FROM customer_orders where order_status='Delivered'";
 
 $run_total_sales = mysqli_query($con,$get_total_sales);
@@ -17,7 +22,7 @@ $row_total_sales = mysqli_fetch_array($run_total_sales);
 
 $total_sales = $row_total_sales['total'];
 
-$get_today_sales = "SELECT sum(due_amount) AS total FROM customer_orders WHERE order_date=DATE(now()) AND order_status='Delivered'";
+$get_today_sales = "SELECT sum(due_amount) AS total FROM customer_orders WHERE CAST(order_date as DATE)='$today' AND order_status in (Order Placed,Delivered,Picked)";
 
 $run_today_sales = mysqli_query($con,$get_today_sales);
 
@@ -33,7 +38,7 @@ $total_count = mysqli_num_rows($run_total_count);
 
 //$total_count = $row_total_count['count'];
 
-$get_today_count = "SELECT DISTINCT invoice_no FROM customer_orders WHERE order_date=DATE(now()) AND order_status='Delivered'";
+$get_today_count = "SELECT DISTINCT invoice_no FROM customer_orders WHERE CAST(order_date as DATE)='$today' AND order_status in (Order Placed,Delivered,Picked)";
 
 $run_today_count = mysqli_query($con,$get_today_count);
 
