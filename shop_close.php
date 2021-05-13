aea7c3b7748c0c874711f41c4eb8881aebd99257
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-05-13 23:10:54 +0530

    error

--- shop_close.php
+++ shop_close.php
@@ -6,7 +6,7 @@
                 <br> <br>
                 <h1 class="text-center" style="font-family:Fredoka One;color:red;">Sorry!</h1>
                 <h6 class="text-center" style="font-family:Josefin Sans;">(Our Deliveries are closed)</h6><br>
-                <h5 class="text-center" style="font-family:Josefin Sans;">To cope up with covid-19 regulations and demand we are improving our delivery services to serve you faster and better. We will be back in next 72 hours to accept orders</h5>
+                <h5 class="text-center" style="font-family:Josefin Sans;">To cope up with covid-19 regulations and demand we are improving our delivery services to serve you faster and better. We will be back by 16th May (Sunday) to accept orders</h5>
                 <br>
                 <h3 class="text-center" style="font-family:Josefin Sans;color:green;">Stay Home | Stay safe</h3>
            </div>
