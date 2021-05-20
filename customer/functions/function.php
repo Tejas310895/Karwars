c5d6316d5c1ac7967f755cb952ca359cd4e131dc
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-05-20 13:29:30 +0530

    Merge branch 'master' of https://github.com/Tejas310895/Karwars

--- customer/functions/function.php
+++ customer/functions/function.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost:3308','root','','wrngrocery');
+$con = mysqli_connect('localhost','u464233779_karwars','Akshay@11','u464233779_karwars');
 
 /// begin getRealIpUser functions ///
 
