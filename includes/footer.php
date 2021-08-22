c5aa7db041f9d068861456bd98c134303c4f65eb
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-08-22 23:04:58 +0530

    site speed

--- includes/footer.php
+++ includes/footer.php
@@ -88,7 +88,7 @@ function showPopForOfflineConnection(){
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="js/script.js?v=1001"></script>
+    <script src="js/script.js?v=1002"></script>
 </body>
 </html>
 <?php ob_end_flush(); ?>
