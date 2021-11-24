3d47927cd9cb5e2f95de9f706c0605568f18529a
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2021-11-24 22:13:33 +0530

    error

--- index.php
+++ index.php
@@ -216,7 +216,7 @@
         <div class="row">
             <div class="col-6">
                 <a href="https://karwars.in/shop?store_id=40">
-                    <img src="https://ik.imagekit.io/wrnear2017/august_upload/SQR_OFFArtboard_100_2x-100_Ve59XOTi9g8.jpg?updatedAt=1635486966141" alt="" class="img-fluid mb-2 mt-2">
+                    <img src="https://ik.imagekit.io/wrnear2017/august_upload/SQR_OFFArtboard_91_2x-100_H7ZQX_csAiP4.jpg?updatedAt=1634986280318" alt="" class="img-fluid mb-2 mt-2">
                 </a>
             </div>
             <div class="col-6">
