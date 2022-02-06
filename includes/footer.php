f98ad362720487040f7e512ec104127b5aed73d3
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2022-02-06 13:04:18 +0530

    scrip optimization async

--- includes/footer.php
+++ includes/footer.php
@@ -1,32 +1,31 @@
-
 <!-- header on scroll -->
-<script src="js/jquery-3.5.1.slim.min.js" ></script>
+<script async src="js/jquery-3.5.1.slim.min.js"></script>
 <script>
     $('#btn-call').click(function() {
-    $('#div-call').animate({width:'toggle'},250);
-});
+        $('#div-call').animate({
+            width: 'toggle'
+        }, 250);
+    });
 </script>
 <script>
-
-$(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 20) {
-            $(".geolocation").addClass("geoactive");
-        } else {
-            //remove the background property so it comes transparent again (defined in your css)
-           $(".geolocation").removeClass("geoactive");
-        }
+    $(function() {
+        $(window).on("scroll", function() {
+            if ($(window).scrollTop() > 20) {
+                $(".geolocation").addClass("geoactive");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+                $(".geolocation").removeClass("geoactive");
+            }
+        });
     });
-});
-
 </script>
 <!-- header on scroll -->
 
 <!-- product carousel script -->
 <!-- Swiper JS -->
-<script src="js/swiper.min.js"></script>
+<!-- <script src="js/swiper.min.js"></script> -->
 <!-- Initialize Swiper -->
-    <script>
+<!-- <script>
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3.4,
         spaceBetween: 65,
@@ -36,18 +35,18 @@ $(function() {
             clickable: true,
         },
         });
-    </script>
+    </script> -->
 <!-- product carousel script -->
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script async src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <script>
-window.addEventListener('resize', function() {
-    if (window.innerWidth >= 700) {
-		window.location = "desktop"; 
-	}
-});
+    window.addEventListener('resize', function() {
+        if (window.innerWidth >= 700) {
+            window.location = "desktop";
+        }
+    });
 </script>
 <!-- <script>
     window.onload = function() {
@@ -56,39 +55,40 @@ window.addEventListener('resize', function() {
     }
 </script> -->
 <script>
-// Add event listener offline to detect network loss.
-window.addEventListener("offline", function(e) {
-    showPopForOfflineConnection();
-});
+    // Add event listener offline to detect network loss.
+    window.addEventListener("offline", function(e) {
+        showPopForOfflineConnection();
+    });
 
-// Add event listener online to detect network recovery.
-window.addEventListener("online", function(e) {
-    hidePopAfterOnlineInternetConnection();
-});
+    // Add event listener online to detect network recovery.
+    window.addEventListener("online", function(e) {
+        hidePopAfterOnlineInternetConnection();
+    });
 
-function hidePopAfterOnlineInternetConnection(){
-    $('#fade-wrapper').fadeOut();
-    // $('#internet-connection-status-dialogue').trigger('close');
-    
-    
-}
+    function hidePopAfterOnlineInternetConnection() {
+        $('#fade-wrapper').fadeOut();
+        // $('#internet-connection-status-dialogue').trigger('close');
 
-function showPopForOfflineConnection(){
-    $('#fade-wrapper').fadeIn();
-    // $('#internet-connection-status-dialogue').lightbox_me({
-    //     centered: true,
-    //     overlaySpeed:"slow",
-    //     closeClick:false,
-    //     onLoad: function() {
-    //     }
-    // });
-}
+
+    }
+
+    function showPopForOfflineConnection() {
+        $('#fade-wrapper').fadeIn();
+        // $('#internet-connection-status-dialogue').lightbox_me({
+        //     centered: true,
+        //     overlaySpeed:"slow",
+        //     closeClick:false,
+        //     onLoad: function() {
+        //     }
+        // });
+    }
 </script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="js/script.js?v=1002"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+<script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script async src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="js/script.js?v=1002"></script>
 </body>
+
 </html>
 <?php ob_end_flush(); ?>
