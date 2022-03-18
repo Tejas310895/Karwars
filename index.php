11979ba0754729ae9811f9009ef7afd3db3c863f
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2022-03-18 18:40:32 +0530

    error

--- index.php
+++ index.php
@@ -186,6 +186,38 @@ include("includes/header.php");
         <span class="sr-only">Next</span>
     </a>
 </div>
+<!-- banner carousel -->
+<div class="container pt-2" style="background-color: #89CFF0;">
+    <div class="row">
+        <div class="col-12">
+            <h5 class="font-weight-bold mb-0 bg-white p-2" style="font-family: Josefin Sans;color:#E34234;">
+                Lowest Price In Karwar
+            </h5>
+        </div>
+        <?php
+
+        $veg_img_array = array(
+            array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_13_2x-100_3__mT_rbEl4P.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646574465563", "shop?store_id=93"),
+            array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_4_2x-100_9pyDI_JTg.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528270", "shop?store_id=115"),
+            array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_2x-100_Bvarja0kiYwu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528820", "shop?store_id=93"),
+            array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_3_2x-100_0Lhwcytlx.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528137", "shop?store_id=106"),
+            array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_2_2x-100_sXLJyUPGh.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528085", "shop?store_id=95"),
+            array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_2x-100_vOG2tCB6P.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528098", "shop?store_id=93")
+        );
+
+        foreach ($veg_img_array as $value) {
+        ?>
+            <div class="col-4 p-2">
+                <a href="<?php echo $value[1];
+                            ?>">
+                    <img src="<?php echo $value[0];
+                                ?>" alt="" class="img-thumbnail" style="border-radius: 10px;">
+                </a>
+            </div>
+        <?php }
+        ?>
+    </div>
+</div>
 <div class="container-fluid p-3 mb-2" style="background-color: #F0FFFF;">
     <div class="row">
         <div class="col-12">
@@ -222,38 +254,6 @@ include("includes/header.php");
         </div>
     </div>
 </div>
-<!-- banner carousel -->
-<!-- <div class="container pt-2" style="background-color: #89CFF0;">
-    <div class="row">
-        <div class="col-12">
-            <h5 class="font-weight-bold mb-0 bg-white p-2" style="font-family: Josefin Sans;color:#E34234;">
-                Lowest Price In Karwar
-            </h5>
-        </div>
-        <?php
-
-        // $veg_img_array = array(
-        //     array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_13_2x-100_3__mT_rbEl4P.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646574465563", "shop?store_id=93"),
-        //     array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_4_2x-100_9pyDI_JTg.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528270", "shop?store_id=115"),
-        //     array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_2x-100_Bvarja0kiYwu.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528820", "shop?store_id=93"),
-        //     array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_3_2x-100_0Lhwcytlx.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528137", "shop?store_id=106"),
-        //     array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_copy_2_2x-100_sXLJyUPGh.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528085", "shop?store_id=95"),
-        //     array("https://ik.imagekit.io/wrnear2017/august_upload/ui_ch_saleArtboard_14_2x-100_vOG2tCB6P.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1646573528098", "shop?store_id=93")
-        // );
-
-        // foreach ($veg_img_array as $value) {
-        ?>
-            <div class="col-4 p-2">
-                <a href="<?php //echo $value[1];
-                            ?>">
-                    <img src="<?php //echo $value[0];
-                                ?>" alt="" class="img-thumbnail" style="border-radius: 10px;">
-                </a>
-            </div>
-        <?php //}
-        ?>
-    </div>
-</div> -->
 
 <div class="container-fluid" style="padding: 25px;">
     <div class="row">
