78a553fdb8399da64ba73bafc806d2705eb4db6b
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2022-05-13 16:03:54 +0530

    error

--- shop_close.php
+++ shop_close.php
@@ -1,16 +1,16 @@
 <?php include("includes/header.php") ?>
-   <div class="container">
-       <div class="row">
-           <div class="col-12 px-4">
-                <img src="admin_area/admin_images/karwarslogo.png" alt="" class="img-thumbnail border-0 mx-auto d-block" width="200px">
-                <br> <br>
-                <h1 class="text-center" style="font-family:Fredoka One;color:red;">Sorry!</h1>
-                <h6 class="text-center" style="font-family:Josefin Sans;">(Our Deliveries are closed)</h6><br>
-                <h5 class="text-center" style="font-family:Josefin Sans;">Due to the new guidelines by the Authorities the deliveries will not be allowed till 24th May, So we will be unable to accept the orders. Sorry for the inconvenience caused</h5>
-                <br>
-                <h3 class="text-center" style="font-family:Josefin Sans;color:green;">Stay Home | Stay safe</h3>
-           </div>
-       </div>
-   </div> 
+<div class="container">
+    <div class="row">
+        <div class="col-12 px-4">
+            <img src="admin_area/admin_images/karwarslogo.png" alt="" class="img-thumbnail border-0 mx-auto d-block" width="200px">
+            <br> <br>
+            <h1 class="text-center" style="font-family:Fredoka One;color:red;">Sorry!</h1>
+            <h6 class="text-center" style="font-family:Josefin Sans;">(Our Deliveries are closed)</h6><br>
+            <h5 class="text-center" style="font-family:Josefin Sans;">Due to technical concerns we are not accepting the orders. Sorry for the inconvenience caused</h5>
+            <br>
+        </div>
+    </div>
+</div>
 </body>
+
 </html>
